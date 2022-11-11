9414f703e4e068941d6ffdc1cdf8057c6b70d52d
Morten Schultz <45948423+mschulzer@users.noreply.github.com>
2022-11-11 12:00:49 +0100

    Create views.py

---
+++ views.py
@@ -0,0 +1,11 @@
+def add_report(request):
+    if request.method == 'POST':
+        form = ReportAdd(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/student/ms@brgym.dk')
+
+    else:
+        form = ReportAdd()
+
+    return render(request, "add_report.html", { 'form': form })
